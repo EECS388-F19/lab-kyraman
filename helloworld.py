be3cbbbcdc272cb638cb9fb5419aa4ec614981e5
kyraman <kyraman@umich.edu>
2019-09-09 12:41:27 -0400

    pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+import random # for random numbers
+
+# print my name
+print('Kyra Mangasarian')
+
+# print two random numbers between 0 and 100
+rand1 = random.randint(0,100)
+rand2 = random.randint(0,100)
+print(rand1)
+print(rand2)
+
+# sum of the two numbers
+print('Sum =', rand1 + rand2)
